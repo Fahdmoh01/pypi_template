9f5e331134ecf2eca56393cbaa7945271e653398
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-10-24 15:17:29 +0530

    reolved echo bugs

--- init_setup.sh
+++ init_setup.sh
@@ -4,6 +4,6 @@ echo [$(date)]: "Creating conda env with python 3.8"
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
-cho [$(date)]: "installing dev requirements"
+echo [$(date)]: "installing dev requirements"
 pip install -r requirements_dev.txt
-cho [$(date)]: "END"
+echo [$(date)]: "END"
